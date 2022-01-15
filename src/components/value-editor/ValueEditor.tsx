633376f02e3e33776f3fc8476fd0d0410182db8f
Trevor <tkhowell95@gmail.com>
2022-01-15 12:29:21 -0700

    Updated how ValueEditor validation works

--- src/components/value-editor/ValueEditor.tsx
+++ src/components/value-editor/ValueEditor.tsx
@@ -29,6 +29,35 @@ const ValueEditor = ({
     inputType = 'checkbox';
   }
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let val: any = e.target.value;
+    if (typeof value === 'number') {
+      val = float ? Number.parseFloat(val) : Number.parseInt(e.target.value);
+      onValueChanged(val);
+    } else {
+      onValueChanged(val);
+    }
+  };
+
+  const validateInput = (val: any) => {
+    if (typeof val !== 'number') {
+      if (inputType === 'checkbox') {
+        onValueChanged(val === 'true');
+      } else {
+        onValueChanged(val);
+      }
+    } else {
+      if (minValue !== undefined && val < minValue) {
+        val = minValue;
+      }
+      if (maxValue !== undefined && val > maxValue) {
+        val = maxValue;
+      }
+
+      onValueChanged(val);
+    }
+  };
+
   return (
     <div className={'value-editor' + (className ? ` ${className}` : '')}>
       {label && <span className="value-label">{label}</span>}
@@ -36,25 +65,9 @@ const ValueEditor = ({
         type={inputType}
         value={value}
         step={step}
-        onChange={e => {
-          let val: any = e.target.value;
-          if (typeof value === 'number') {
-            val = float ? Number.parseFloat(val) : Number.parseInt(e.target.value);
-
-            if (minValue !== undefined && val < minValue) {
-              val = minValue;
-            }
-            if (maxValue !== undefined && val > maxValue) {
-              val = maxValue;
-            }
-
-            onValueChanged(val);
-          } else if (inputType === 'checkbox') {
-            onValueChanged(val === 'true');
-          } else {
-            onValueChanged(e.target.value);
-          }
-        }}
+        onSubmit={onChange}
+        onChange={onChange}
+        onBlur={() => validateInput(value)}
       />
     </div>
   );
